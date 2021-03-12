5d096be97f998c4598538304b8d1b27e4ae98038
kamil-alperen <kamilalperenyilmaz11235@gmail.com>
2021-03-12 21:36:16 +0300

    App class has changed

--- src/main/java/project/App.java
+++ src/main/java/project/App.java
@@ -29,7 +29,10 @@ public class App {
     }
 
     public static void main(String[] args) {
-        port(getHerokuAssignedPort());
+        Logger logger = LogManager.getLogger(App.class);
+        int port = Integer.parseInt(System.getenv("PORT"));
+        port(port);
+        logger.error("Current port number:" + port);
 
         get("/", (req, res) -> "Naber Irak iran");
 
