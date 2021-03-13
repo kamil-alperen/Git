307d7573a2b59b6155fa39d986e4e8e6a716b168
kamil-alperen <kamilalperenyilmaz11235@gmail.com>
2021-03-13 14:31:06 +0300

    my application

--- src/main/java/project/App.java
+++ src/main/java/project/App.java
@@ -9,7 +9,9 @@ import static spark.Spark.post;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -18,23 +20,58 @@ import spark.ModelAndView;
 import spark.template.mustache.MustacheTemplateEngine;
 
 public class App {
-    public static int average(ArrayList<Integer> array) {
-      System.out.println("inside search");
-      if (array == null) return 0;
-      int total = 0;
-      for (int elt : array) {
-        total += elt;
+    public static String determineNote(int score,int limitation,int standard_deviation){
+        if(score >= limitation + standard_deviation*6)return "AA";
+        else if(score >= limitation + standard_deviation*5)return "BA";
+        else if(score >= limitation + standard_deviation*4)return "BB";
+        else if(score >= limitation + standard_deviation*3)return "CB";
+        else if(score >= limitation + standard_deviation*2)return "CC";
+        else if(score >= limitation + standard_deviation)return "DC";
+        else if(score >= limitation)return "DD";
+        else return "FF";
+    }
+    public static String orderAndScoreStudents(ArrayList<String> students,ArrayList<Integer> scores,Integer limitation, Integer standard_deviation) {
+      if(students == null || scores == null)return "Please enter some student/score inputs";
+      if(limitation <= 0 | limitation > 100)return "Please enter valid positive limitation";
+      if(standard_deviation <= 0 | standard_deviation > 100)return "Please enter valid positive standard deviation";
+      int length = Math.min(students.size(), scores.size());
+      Map<String,Integer> map = new HashMap<>();
+      for(int i = 0;i < length;i++){
+        map.put(students.get(i), scores.get(i));
       }
-      return total / array.size();
+      map = map.entrySet().stream().sorted(Map.Entry.comparingByValue()).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,(e1,e2) -> e1, LinkedHashMap::new));
+      String message = "";
+      for(Map.Entry<String,Integer> entry : map.entrySet()){
+          String note = determineNote(entry.getValue(), limitation, standard_deviation);
+          if(!note.equals("FF")){
+            message += entry.getKey()+" got "+note+" and passed the lesson\n";
+          }
+          else message += entry.getKey()+" got "+note+" and failed the lesson\n";
+      }
+      if(students.size() > length)message += "\nNote:\n";
+      for(int i = length;i < students.size();i++){
+        message += "There is not score information about the student : "+students.get(i)+"\n";
+      }
+      if(students.size() == length & scores.size() > length){
+        message += "\nNote:\n";
+        for(int i = length;i < scores.size();i++){
+          message += "There is not student who takes : "+scores.get(i)+"\n";
+        }
+      }
+      return message;
     }
 
     public static void main(String[] args) {
-        Logger logger = LogManager.getLogger(App.class);
-        int port = Integer.parseInt(System.getenv("PORT"));
-        port(port);
-        logger.error("Current port number:" + port);
+        //Logger logger = LogManager.getLogger(App.class);
+        //int port = Integer.parseInt(System.getenv("PORT"));
+        //port(port);
+        //logger.error("Current port number:" + port);
+        int number = getHerokuAssignedPort();
+        port(number);
+
+        System.out.println("Port number is : "+number);
         
-        get("/", (req, res) -> "Welcome To My Website !");
+        get("/", (req, res) -> "Welcome To My Website !\nPlease add compute to URL to access the application");
 
         MustacheTemplateEngine engine = new MustacheTemplateEngine();
         get("/compute",
@@ -48,18 +85,35 @@ public class App {
           String input1 = req.queryParams("input1");
           java.util.Scanner sc1 = new java.util.Scanner(input1);
           sc1.useDelimiter("[;\r\n]+");
-          java.util.ArrayList<Integer> inputList = new java.util.ArrayList<>();
+          java.util.ArrayList<String> inputList = new java.util.ArrayList<>();
           while (sc1.hasNext())
           {
-            int value = Integer.parseInt(sc1.next().replaceAll("\\s",""));
-            inputList.add(value);
+            String student = sc1.next();
+            inputList.add(student);
           }
           sc1.close();
           System.out.println(inputList);
 
-          int result = App.average(inputList);
+          String input2 = req.queryParams("input2");
+          java.util.Scanner sc2 = new java.util.Scanner(input2);
+          sc2.useDelimiter("[;\r\n]+");
+          java.util.ArrayList<Integer> inputList2 = new java.util.ArrayList<>();
+          while (sc2.hasNext())
+          {
+            int value = Integer.parseInt(sc2.next().replaceAll("\\s",""));
+            inputList2.add(value);
+          }
+          sc2.close();
+          System.out.println(inputList2);
 
-          Map<String, Integer> map = new HashMap<String, Integer>();
+          String input3 = req.queryParams("input3").replaceAll("\\s","");
+          int limitation = Integer.parseInt(input3);
+          String input4 = req.queryParams("input4").replaceAll("\\s","");
+          int standard_deviation = Integer.parseInt(input4);
+         
+          // get result
+          String result = orderAndScoreStudents(inputList, inputList2, limitation, standard_deviation);
+          Map<String, String> map = new HashMap<String, String>();
           map.put("result", result);
           return new ModelAndView(map, "compute.mustache");
         }, new MustacheTemplateEngine());
--- src/test/java/project/AppTest.java
+++ src/test/java/project/AppTest.java
@@ -6,6 +6,133 @@ package project;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+
+import javax.lang.model.util.ElementScanner14;
+
 class AppTest { 
-    
+    ArrayList<String> students;
+    ArrayList<Integer> scores;
+    int limitation;
+    int standard_deviation;
+    @Test void studentsNull(){
+        students = null;
+        scores = new ArrayList<>();
+        for(int i = 0;i < 10;i++)
+            scores.add(i);
+        limitation = 60;
+        standard_deviation = 5;
+        String message = "Please enter some student/score inputs";
+        assertEquals(message, App.orderAndScoreStudents(students, scores, limitation, standard_deviation));
+        
+    }
+    @Test void scoresNull(){
+        students = new ArrayList<>();
+        students.add("alperen yilmaz");
+        students.add("murat genc");
+        scores = null;
+        limitation = 60;
+        standard_deviation = 5;
+        String message = "Please enter some student/score inputs";
+        assertEquals(message, App.orderAndScoreStudents(students, scores, limitation, standard_deviation));
+    }
+    @Test void limitation_isNegative(){
+        students = new ArrayList<>();
+        students.add("alperen yilmaz");
+        students.add("murat genc");
+        scores = new ArrayList<>();
+        scores.add(10);
+        scores.add(20);
+        limitation = -10;
+        standard_deviation = 5;
+        String message = "Please enter valid positive limitation";
+        assertEquals(message, App.orderAndScoreStudents(students, scores, limitation, standard_deviation));
+    }
+    @Test void limitation_isAbove100(){
+        students = new ArrayList<>();
+        students.add("alperen yilmaz");
+        students.add("murat genc");
+        scores = new ArrayList<>();
+        scores.add(10);
+        scores.add(20);
+        limitation = 110;
+        standard_deviation = 5;
+        String message = "Please enter valid positive limitation";
+        assertEquals(message, App.orderAndScoreStudents(students, scores, limitation, standard_deviation));
+    }
+    @Test void standardDeviation_isNegative(){
+        students = new ArrayList<>();
+        students.add("alperen yilmaz");
+        students.add("murat genc");
+        scores = new ArrayList<>();
+        scores.add(20);
+        scores.add(40);
+        limitation = 60;
+        standard_deviation = -5;
+        String message = "Please enter valid positive standard deviation";
+        assertEquals(message, App.orderAndScoreStudents(students, scores, limitation, standard_deviation));
+    }
+    @Test void standardDeviation_isAbove100(){
+        students = new ArrayList<>();
+        students.add("alperen yilmaz");
+        students.add("murat genc");
+        scores = new ArrayList<>();
+        scores.add(20);
+        scores.add(40);
+        limitation = 60;
+        standard_deviation = 105;
+        String message = "Please enter valid positive standard deviation";
+        assertEquals(message, App.orderAndScoreStudents(students, scores, limitation, standard_deviation));
+    }
+    @Test void Students_biggerThan_Scores(){
+        students = new ArrayList<>();
+        students.add("alperen yilmaz");
+        students.add("murat genc");
+        students.add("busra genc");
+        scores = new ArrayList<>();
+        scores.add(20);
+        scores.add(70);
+        limitation = 60;
+        standard_deviation = 5;
+        String message = App.orderAndScoreStudents(students, scores, limitation, standard_deviation);
+        if(message.contains("There is not score information about the student"))
+            assertTrue(true);
+        else    
+            assertTrue(false);
+    }
+    @Test void Scores_biggerThan_students(){
+        students = new ArrayList<>();
+        students.add("alperen yilmaz");
+        students.add("murat genc");
+        scores = new ArrayList<>();
+        scores.add(20);
+        scores.add(80);
+        scores.add(50);
+        limitation = 60;
+        standard_deviation = 5;
+        String message = App.orderAndScoreStudents(students, scores, limitation, standard_deviation);
+        if(message.contains("There is not student who takes"))
+            assertTrue(true);
+        else    
+            assertTrue(false);
+    } 
+    @Test void Scores_equals_students(){
+        students = new ArrayList<>();
+        students.add("alperen yilmaz");
+        students.add("murat genc");
+        students.add("busra kerdonmez");
+        scores = new ArrayList<>();
+        scores.add(66);
+        scores.add(82);
+        scores.add(76);
+        limitation = 60;
+        standard_deviation = 5;
+        String message = App.orderAndScoreStudents(students, scores, limitation, standard_deviation);
+        if(message.contains("There is not score information about the student"))
+            assertTrue(false);
+        else if(message.contains("There is not student who takes"))
+            assertTrue(false);
+        else 
+            assertTrue(true);
+    }
 }
